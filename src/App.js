8eaec6b4b5ccd941d1959913788ef3cfdd5c4b1b
co4s1 <danielsk306@gmail.com>
2026-09-20 13:57:27 +0200

    minor design improvements

--- src/App.js
+++ src/App.js
@@ -35,13 +35,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <h3>Wobler-jpt</h3>
+        </div>
         <div className="outputdiv">
           <span className="outputspan">
-            <p className="outputtext">{(output !== "") ? output : 'Nothing to see here'}</p>
+            <p className="outputtext">{(output !== "") ? output : 'Nothing to see here. Provide wobler-jpt with the beginning of the output string :)'}</p>
           </span>
         </div>
         <div className="inputdiv">
-          <input className="inputinput" placeholder="Beggining of the output" onChange={e => setInput(e.target.value)}></input>
+          <input className="inputinput" placeholder="Enter the beggining of the output" onChange={e => setInput(e.target.value)}></input>
           <button className="sendbutton" onClick={() => sendMessage(input)} disabled={isLoading}>
             {isLoading ? '...' : 'Enter'}
           </button>
